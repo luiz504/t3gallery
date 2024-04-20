fc0783b91871325168d94d9bda2b14821b2c66e7
luiz bueno <luiz504@hotmail.com>
2024-04-20 15:59:02 -0300

    chore: files eslint fix

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,14 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { sql } from 'drizzle-orm'
 import {
   index,
   pgTableCreator,
   serial,
   timestamp,
   varchar,
-} from "drizzle-orm/pg-core";
+} from 'drizzle-orm/pg-core'
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -16,19 +16,19 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
+export const createTable = pgTableCreator((name) => `t3gallery_${name}`)
 
 export const posts = createTable(
-  "post",
+  'post',
   {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 256 }),
+    createdAt: timestamp('created_at')
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updatedAt"),
+    updatedAt: timestamp('updatedAt'),
   },
   (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+    nameIndex: index('name_idx').on(example.name),
+  }),
+)
